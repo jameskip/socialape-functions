7f3e8f306b86d7116ae8ddfe0626be2cc1aa13cf
James Kip <jcontreras@thrivehive.com>
2019-11-26 12:54:46 -0700

    Add signup validation

--- functions/index.js
+++ functions/index.js
@@ -60,6 +60,16 @@ app.post('/scream', (request, response) => {
     })
 })
 
+const isEmpty = string => string.trim() === '' ? true : false
+const isValidEmail = email => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if (email.match(regEx)) {
+    return true
+  } else {
+    return false
+  }
+}
+
 app.post('/signup', (request, response) => {
   const newUser = {
     email: request.body.email,
@@ -67,6 +77,20 @@ app.post('/signup', (request, response) => {
     confirmPassword: request.body.confirmPassword,
     handle: request.body.handle
   }
+
+  let errors = {}
+
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty.'
+  } else if (!isValidEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address.'
+  }
+
+  if (isEmpty(newUser.password)) errors.password = 'Must not be empty'
+  if (newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match'
+  if (isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+  if (Object.keys(errors).length > 0) return response.status(400).json({ errors })
 
   let token, userId
   db.doc(`/users/${newUser.handle}`).get()

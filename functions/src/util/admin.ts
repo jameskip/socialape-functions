fa4feefe11aaa7f2c6f3bb2d2583a1b049912a6d
James Kip <jcontreras@thrivehive.com>
2020-02-16 15:56:49 -0700

    Add removed files

---
+++ functions/src/util/admin.ts
@@ -0,0 +1,12 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./socialape-9a7bf-firebase-adminsdk-1jogg-fe96800ee7.json");
+
+// Init Firebase database
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://socialape-9a7bf.firebaseio.com"
+});
+
+const db = admin.firestore();
+
+module.exports = { admin, db };
